e4f4599d2da485c20f51324b936166bc165f3936
AlbertoFormaggio1 <alb.formaggio@gmail.com>
2023-11-18 14:24:04 +0100

    Generalized evaluation function

--- engine.py
+++ engine.py
@@ -18,19 +18,19 @@ def train(model, train_ds, val_ds, loss_fn: torch.nn.Module,
 
         # every 10 epochs see the improvement on the validation set
         if epoch % 10 == 0:
-            val_loss, val_acc = eval(model, loss_fn, val_ds)
+            val_loss, val_acc = eval(model, loss_fn, val_ds, val_ds.val_mask)
             results['val_loss'].append(val_loss)
             results['val_acc'].append(val_acc)
 
     return results
 
 
-def eval(model, loss_fn, val_ds):
+def eval(model, loss_fn, ds, mask):
     with torch.no_grad():
-        out = model(val_ds.x, val_ds.edge_index)
-        loss = loss_fn(out[val_ds.val_mask], val_ds.y[val_ds.val_mask]).item()
+        out = model(ds.x, ds.edge_index)
+        loss = loss_fn(out[mask], ds.y[mask]).item()
         cls = out.argmax(dim=-1)
-        acc = torch.sum(cls[val_ds.val_mask] == val_ds.y[val_ds.val_mask]) / torch.sum(val_ds.val_mask)
+        acc = torch.sum(cls[mask] == ds.y[mask]) / torch.sum(mask)
 
     return loss, acc.item()
 
