6e5ad0a38b0398e5c8b000ea762cc8cfba68a9dc
AlbertoFormaggio1 <alb.formaggio@gmail.com>
2023-11-22 14:29:02 +0100

    Added comments

--- engine.py
+++ engine.py
@@ -12,7 +12,9 @@ def train(model, train_ds, val_ds, loss_fn: torch.nn.Module,
                "val_acc": []
                }
 
-    # whether we need to use some loader to generate batches or we use the full-batch approach
+    # whether we need to use some loader to generate batches or we use the full-batch approach.
+    # GraphSAGE works with generation of batches where you only pick a subset of the original graph
+    # The other networks instead don't need that
     if batch_generation:
         # We keep the 25 neighbors of each node and then 10 neighbors for each of them
         # They trained on 10 epochs for the fully supervised sampling
@@ -26,13 +28,17 @@ def train(model, train_ds, val_ds, loss_fn: torch.nn.Module,
         for batch in train_batches:
             cur_train_loss, cur_train_acc = train_step(model=model, ds=batch, loss_fn=loss_fn, opt=opt)
             if batch_generation:
+                # If we generated batches, than we get the size by doing batch.batch_size
                 batch_size = batch.batch_size
             else:
+                # Otherwise, we sum the values to 1 in the training mask (training mask has a 1 for every node to consider in the training set)
                 batch_size = torch.sum(batch.train_mask).item()
             batch_num += batch_size
+            # Increase the loss and the accuracy propotionally to the size of the batch
             train_loss += cur_train_loss * float(batch_size)
             train_acc += cur_train_acc * float(batch_size)
 
+        # Compute the average loss and accuracy
         train_loss /= batch_num
         train_acc /= batch_num
 
@@ -49,9 +55,13 @@ def train(model, train_ds, val_ds, loss_fn: torch.nn.Module,
 
 def eval(model, loss_fn, ds, mask):
     with torch.no_grad():
+        # Compute the response of the model
         out = model(ds.x, ds.edge_index)
+        # Compute the loss function and with item() get the numerical value associated to it
         loss = loss_fn(out[mask], ds.y[mask]).item()
+        # Compute the argmax over the outputs (you may apply softmax as well, but it's just a scaling)
         cls = out.argmax(dim=-1)
+        # Compute the number of correctly classified samples
         acc = torch.sum(cls[mask] == ds.y[mask]) / torch.sum(mask)
 
     return loss, acc.item()
@@ -76,9 +86,18 @@ def train_step(model: torch.nn.Module, ds, loss_fn: torch.nn.Module,
 
 def train_link_prediction(model, train_ds, loss_fn: torch.nn.Module,
           opt: torch.optim.Optimizer, epochs: int):
+    """
+    This function trains a link predictor model
+    :param model:
+    :param train_ds:
+    :param loss_fn:
+    :param opt:
+    :param epochs:
+    :return:
+    """
 
     for epoch in tqdm(range(epochs)):
-        model.train()
+        model.train() # Set the model in training phase
         opt.zero_grad()
         # Computing first the embeddings with message passing on the edges that are already existing
         # in the graph
@@ -110,9 +129,16 @@ def train_link_prediction(model, train_ds, loss_fn: torch.nn.Module,
 
     return loss.item()
 
+"""
+This eval_predictor may be included into eval.
+The difference is the following: eval_predictor is computing the performance (AUC) of the link predictor
+eval instead computes the accuracy of the classifier
+"""
 @torch.no_grad()
 def eval_predictor(model, data):
-    model.eval()
-    z = model(data.x, data.edge_index)
+    model.eval()    # Set the model to evaluation stage
+    z = model(data.x, data.edge_index)  # Compute the embeddings
+    # Perform the decoding, flatten it by using view(-1) and then compute the confidence with the sigmoid activation function
     out = model.decode(z, data.edge_label_index).view(-1).sigmoid()
+    # Compute the AUC score
     return roc_auc_score(data.edge_label.cpu().numpy(), out.cpu().numpy())
