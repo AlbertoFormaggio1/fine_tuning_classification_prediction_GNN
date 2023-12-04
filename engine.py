12ce2b4ebb19b517a5d826debf3fe5b7c9af13bc
AlbertoFormaggio1 <alb.formaggio@gmail.com>
2023-12-04 22:18:47 +0100

    Accuracy and Loss Corrected for SAGE

--- engine.py
+++ engine.py
@@ -31,12 +31,8 @@ def train(model, train_ds, val_ds, loss_fn: torch.nn.Module,
         batch_num = 0
         for batch in train_batches:
             cur_train_loss, cur_train_acc = train_step(model=model, ds=batch, loss_fn=loss_fn, opt=opt)
-            if batch_generation:
-                # If we generated batches, than we get the size by doing batch.batch_size
-                batch_size = batch.batch_size
-            else:
-                # Otherwise, we sum the values to 1 in the training mask (training mask has a 1 for every node to consider in the training set)
-                batch_size = torch.sum(batch.train_mask).item()
+            # Otherwise, we sum the values to 1 in the training mask (training mask has a 1 for every node to consider in the training set)
+            batch_size = torch.sum(batch.train_mask).item()
             batch_num += batch_size
             # Increase the loss and the accuracy propotionally to the size of the batch
             train_loss += cur_train_loss * float(batch_size)
@@ -50,17 +46,18 @@ def train(model, train_ds, val_ds, loss_fn: torch.nn.Module,
         results['train_acc'].append(train_acc)
 
         # every 10 epochs see the improvement on the validation set
-        if epoch % 10 == 0:
+        if epoch % 20 == 0:
             val_loss, val_acc = eval_classifier(model, loss_fn, val_ds, True, batch_generation)
             results['val_loss'].append(val_loss)
             results['val_acc'].append(val_acc)
 
     return results
 
+
 def train_step(model: torch.nn.Module, ds, loss_fn: torch.nn.Module,
                opt: torch.optim.Optimizer):
     model.train()  # Set the model in training mode
-    opt.zero_grad() # Reset the gradient
+    opt.zero_grad()  # Reset the gradient
     out = model(ds.x, ds.edge_index)  # Compute the response of the model
     loss = loss_fn(out[ds.train_mask], ds.y[ds.train_mask])  # Compute the loss based on training nodes
     loss.backward()  # Propagate the gradient
